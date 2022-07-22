c65a62a6744742603c5c919e4c7c503431286174
SamuelFST <guti.samuel2002@gmail.com>
2022-07-22 20:52:07 -0300

    feat: routes for the SignIn, SignUp and home pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <SignIn />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<SignIn/>} />
+        <Route path='/home' element={<Home />} />
+        <Route path='/register' element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
