afdf9f1708d72ae023f5f7127dba54863d31c518
SamuelFST <guti.samuel2002@gmail.com>
2022-07-25 19:17:35 -0300

    chore: routes for NewPost, Profile and Profiles pages, and ToastContainer

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Home from './pages/Home';
+import NewPost from './pages/NewPost';
+import Profile from './pages/Profile';
+import Profiles from './pages/Profiles';
 
 function App() {
   return (
@@ -12,7 +17,11 @@ function App() {
         <Route path='/' element={<SignIn/>} />
         <Route path='/home' element={<Home />} />
         <Route path='/register' element={<SignUp />} />
+        <Route path='/create' element={<NewPost />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/profiles' element={<Profiles />} />
       </Routes>
+      <ToastContainer autoClose={5000} className="toast-container" />
     </BrowserRouter>
   );
 }
