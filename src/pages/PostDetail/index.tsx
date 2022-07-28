521db6b3d3d6b435ae3536468c004c86a0406107
SamuelFST <guti.samuel2002@gmail.com>
2022-07-28 16:28:47 -0300

    chore: post comments

--- src/pages/PostDetail/index.tsx
+++ src/pages/PostDetail/index.tsx
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from 'react-router-dom';
 import { toast } from "react-toastify";
-import { Divider, TextField, Paper, Button } from "@mui/material";
+import { Divider, TextField, Paper, Button, CardHeader } from "@mui/material";
 
 import { Post } from "../../models/Post";
 import axios from "../../api/axios";
 import Header from "../../components/Header";
 import PostCard from "../../components/PostCard";
+import CustomAvatar from "../../components/CustomAvatar";
 
 const PostDetail = () => {
   const { postId } = useParams();
   const token = localStorage.getItem('accessToken');
+  const profileId = localStorage.getItem('profile');
+  const profileName = localStorage.getItem('user');
   const [post, setPost] = useState<Post>();
   const [comment, setComment] = useState({ value: "", error: "" });
 
@@ -35,15 +38,22 @@ const PostDetail = () => {
     e.preventDefault();
     try {
       const response = await axios.post(`/posts/${postId}/comments`,
-      { description: comment.value },
-      {
-        headers: {
-          authorization: `Bearer ${token}`,
-        },
-      });
+        { description: comment.value },
+        {
+          headers: {
+            authorization: `Bearer ${token}`,
+          },
+        });
 
-      setComment({...comment, value: ""});
-      post?.comments.push(response.data);
+      setComment({ ...comment, value: "" });
+      const newComment = {
+        ...response.data,
+        profile: {
+          _id: profileId,
+          name: profileName,
+        }
+      }
+      post?.comments.push(newComment);
       setPost(post);
     } catch (err) {
       toast.error('Ocorreu um erro ao adicionar um comentário');
@@ -82,6 +92,20 @@ const PostDetail = () => {
           </div>
         </form>
       </Paper >
+      <Divider sx={{ marginTop: 2 }} />
+      <div>
+        {post?.comments && post?.comments.map((item) => (
+          <div key={item._id}>
+            <Paper elevation={0} sx={{ marginX: 24, marginTop: 2 }}>
+              <CardHeader
+                avatar={<CustomAvatar profileName={item.profile.name} />}
+                title={item.description}
+              />
+              <Divider />
+            </Paper>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
