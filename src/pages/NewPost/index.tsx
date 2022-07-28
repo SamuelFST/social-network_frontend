312257a95f3e976d833deb57145e844fd351b93a
SamuelFST <guti.samuel2002@gmail.com>
2022-07-28 16:33:04 -0300

    chore: newPost inputs and button

--- src/pages/NewPost/index.tsx
+++ src/pages/NewPost/index.tsx
@@ -1,11 +1,33 @@
 import React from 'react';
+import { TextField, Stack, Container, Button } from '@mui/material';
+
 import Header from '../../components/Header';
+import Dropzone from '../../components/Dropzone';
 
 const NewPost = () => {
   return (
     <div>
       <Header title='Nova Postagem' />
-      <h1 style={{ marginTop: "100px" }}>Nova postagem</h1>
+      <Container sx={{ marginTop: 12 }}>
+        <Stack spacing={6}>
+          <TextField
+            variant='standard'
+            label='Título'
+            name='title'
+          />
+          <TextField
+            variant='standard'
+            label='O que está acontecendo?'
+            name='description'
+            multiline
+            minRows={3}
+          />
+          <Dropzone onFileUploaded={() => {}} />
+          <Button variant='contained' type='submit'>
+            Publicar
+          </Button>
+        </Stack>
+      </Container>
     </div>
   );
 }
